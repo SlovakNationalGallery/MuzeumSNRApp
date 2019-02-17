da8abb83051c26c309ff5195afc666f551c568fc
Igor Rjabinin <igor.rjabinin@gmail.com>
2019-02-17 02:42:29 +0100

    display documents from array

--- src/app/dokumenty/dokumenty.page.ts
+++ src/app/dokumenty/dokumenty.page.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DokumentyPage implements OnInit {
 
-  constructor() { }
+  pdf_files = [
+    { year: "1847", title: "Reč Ľudovíta Štúra na Uhorskom sneme"},
+    { year: "1848", title: "Žiadosti slovenského národa"},
+    { year: "1918", title: "Rezolúcia"},
+    { year: "1920", title: "Ústava Československej republiky"}
+  ];
+
+
+  files_per_row = 2;
+  pdf_files_in_rows = [];
+
+  constructor() {
+  }
 
   ngOnInit() {
+    for (let i = 0; i < this.pdf_files.length; i += this.files_per_row) {
+      this.pdf_files_in_rows.push({ items: this.pdf_files.slice(i, i + this.files_per_row ) });
+    }
   }
 
 }
