2178e078b127ea5b0328f111003399b475ee55aa
Igor Rjabinin <igor.rjabinin@gmail.com>
2019-02-26 12:14:33 +0100

    add lodash + display statne-symboly grouped by years

--- src/app/statne-symboly/statne-symboly.page.ts
+++ src/app/statne-symboly/statne-symboly.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
 import symboly_data from '../../assets/data/statne-symboly.json';
 
 @Component({
@@ -11,10 +12,15 @@ export class StatneSymbolyPage implements OnInit {
   symboly_data = [];
   data_per_row = 2;
   symboly_data_in_rows = [];
+  symboly_data_by_year = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.symboly_data = symboly_data;
+    this.symboly_data_by_year = _.groupBy(symboly_data, "year");
+    console.log(this.symboly_data_by_year);
+
     for (let i = 0; i < symboly_data.length; i += this.data_per_row) {
       this.symboly_data_in_rows.push({ items: symboly_data.slice(i, i + this.data_per_row ) });
     }
